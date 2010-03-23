e0205f03aac6eb9368bb61d2e7376b8f31b56e20
Rajiv Aaron Manglani <rajiv@brontes3d.com>
2010-03-23 13:33:54 -0400

    fix rails deprecations.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
 require 'rubygems'
 require 'test/unit'
-require 'activerecord'
+require 'active_record'
 
 #require this plugin
 require File.join(File.dirname(__FILE__), "..", "init")
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :dbfile => ":memory:")
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 
 #load the database schema for this test
 load File.expand_path(File.dirname(__FILE__) + "/test_models/schema.rb")
